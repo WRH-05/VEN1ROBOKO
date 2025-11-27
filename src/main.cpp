1b0137f9a962e3965a61da644a67561aa60dbf9a
Wassim <wassimhachemi8@gmail.com>
2025-11-27 18:27:56 +0100

    adding motors functions(not tested yet)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
+#include <AFMotor.h>
 
 const int trigPin = 2;       // Shared Trigger D2
 const int echoPinFront = A0; // Front Echo A0
 const int echoPinLeft = A5;  // Left Echo A5
 const int echoPinRight = A1; // Right Echo A1
+
+// Motor setup
+AF_DCMotor motorLeft(1);   // M1
+AF_DCMotor motorRight(2);  // M2
 
 void setup() {
   Serial.begin(9600);
@@ -12,6 +17,11 @@ void setup() {
   pinMode(echoPinFront, INPUT);
   pinMode(echoPinLeft, INPUT);
   pinMode(echoPinRight, INPUT);
+  
+  // Set initial motor speed
+  motorLeft.setSpeed(255);
+  motorRight.setSpeed(255);
+  
   Serial.println("Triple Ultrasonic Sensor Test Started");
 }
 
@@ -57,6 +67,7 @@ void loop() {
   Serial.print("Front: ");
   if (distFront == -1) {
     Serial.print("Error");
+    distFront = 999; // Set to high value if error
   } else {
     Serial.print(distFront);
     Serial.print(" cm");
@@ -65,6 +76,7 @@ void loop() {
   Serial.print(" | Right: ");
   if (distRight == -1) {
     Serial.print("Error");
+    distRight = 999; // Set to high value if error
   } else {
     Serial.print(distRight);
     Serial.print(" cm");
@@ -73,11 +85,57 @@ void loop() {
   Serial.print(" | Left: ");
   if (distLeft == -1) {
     Serial.print("Error");
+    distLeft = 999; // Set to high value if error
   } else {
     Serial.print(distLeft);
     Serial.print(" cm");
   }
   Serial.println();
 
-  delay(500);
+  // Find closest sensor and distance
+  long minDist = distFront;
+  String direction = "FRONT";
+  
+  if (distRight < minDist) {
+    minDist = distRight;
+    direction = "RIGHT";
+  }
+  
+  if (distLeft < minDist) {
+    minDist = distLeft;
+    direction = "LEFT";
+  }
+
+  // Motor control logic
+  if (minDist < 77) { // Less than 770mm (77cm)
+    // Move towards closest object at max speed
+    motorLeft.setSpeed(255);
+    motorRight.setSpeed(255);
+    
+    if (direction == "FRONT") {
+      // Move forward
+      motorLeft.run(FORWARD);
+      motorRight.run(FORWARD);
+      Serial.println("Action: Moving FORWARD");
+    } else if (direction == "RIGHT") {
+      // Turn right
+      motorLeft.run(FORWARD);
+      motorRight.run(BACKWARD);
+      Serial.println("Action: Turning RIGHT");
+    } else if (direction == "LEFT") {
+      // Turn left
+      motorLeft.run(BACKWARD);
+      motorRight.run(FORWARD);
+      Serial.println("Action: Turning LEFT");
+    }
+  } else {
+    // No object detected below 770mm, rotate slowly
+    motorLeft.setSpeed(100);
+    motorRight.setSpeed(100);
+    motorLeft.run(FORWARD);
+    motorRight.run(BACKWARD);
+    Serial.println("Action: Rotating SLOWLY");
+  }
+
+  delay(100);
 }
